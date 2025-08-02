1ed896867e7b38f8f79e748966a86ca27869f698
idan8426 <idan8426@gmail.com>
2025-08-02 19:38:12 +0300

    Implemented main menu

---
+++ SweetRelease/Assets/Project/Scripts/MenuParallax.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Project.Scripts
+{
+    public class MenuParallax : MonoBehaviour
+    {
+        [SerializeField]
+        private float offsetMultiplier = 1f;
+
+        [SerializeField]
+        private float smoothTime = .3f;
+
+        private Vector2 startPosition;
+        private Transform m_cachedTransform;
+        private Vector3 velocity;
+
+        private void Start()
+        {
+            startPosition = transform.position;
+            m_cachedTransform = transform;
+        }
+
+        private void Update()
+        {
+            Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            m_cachedTransform.position = Vector3.SmoothDamp(m_cachedTransform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+        }
+    }
+}
